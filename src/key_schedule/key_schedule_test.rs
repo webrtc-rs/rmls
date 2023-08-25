04e6c5e2f7cef3c878d4cc6935260b5d0a438c6a
yngrtc <y@ngr.tc>
2023-08-24 21:50:13 -0700

    pass test_key_schedule

--- src/key_schedule/key_schedule_test.rs
+++ src/key_schedule/key_schedule_test.rs
@@ -3,9 +3,15 @@ use serde::{Deserialize, Serialize};
 
 use crate::cipher_suite::CipherSuite;
 use crate::codec::codec_test::load_test_vector;
+use crate::codec::write;
 use crate::crypto::provider::{ring::RingCryptoProvider, rust::RustCryptoProvider, CryptoProvider};
 use crate::error::*;
-use crate::key_schedule::{extract_psk_secret, PreSharedKeyID, Psk};
+use crate::framing::PROTOCOL_VERSION_MLS10;
+use crate::key_schedule::{
+    extract_psk_secret, extract_welcome_secret, GroupContext, PreSharedKeyID, Psk,
+    SECRET_LABEL_CONFIRM, SECRET_LABEL_ENCRYPTION, SECRET_LABEL_EXPORTER, SECRET_LABEL_EXTERNAL,
+    SECRET_LABEL_INIT, SECRET_LABEL_MEMBERSHIP, SECRET_LABEL_RESUMPTION, SECRET_LABEL_SENDER_DATA,
+};
 
 #[derive(Default, Debug, Clone, Serialize, Deserialize)]
 struct PSK {
@@ -73,126 +79,159 @@ fn test_psk_secret() -> Result<()> {
 
     Ok(())
 }
-/*
-type keyScheduleTest struct {
-    CipherSuite cipherSuite `json:"cipher_suite"`
 
-    GroupID           testBytes `json:"group_id"`
-    InitialInitSecret testBytes `json:"initial_init_secret"`
-
-    Epochs []struct {
-        TreeHash                testBytes `json:"tree_hash"`
-        CommitSecret            testBytes `json:"commit_secret"`
-        PSKSecret               testBytes `json:"psk_secret"`
-        ConfirmedTranscriptHash testBytes `json:"confirmed_transcript_hash"`
-
-        GroupContext testBytes `json:"group_context"`
-
-        JoinerSecret  testBytes `json:"joiner_secret"`
-        WelcomeSecret testBytes `json:"welcome_secret"`
-        InitSecret    testBytes `json:"init_secret"`
-
-        SenderDataSecret   testBytes `json:"sender_data_secret"`
-        EncryptionSecret   testBytes `json:"encryption_secret"`
-        ExporterSecret     testBytes `json:"exporter_secret"`
-        EpochAuthenticator testBytes `json:"epoch_authenticator"`
-        ExternalSecret     testBytes `json:"external_secret"`
-        ConfirmationKey    testBytes `json:"confirmation_key"`
-        MembershipKey      testBytes `json:"membership_key"`
-        ResumptionPSK      testBytes `json:"resumption_psk"`
-
-        ExternalPub testBytes `json:"external_pub"`
-        Exporter    struct {
-            Label   string    `json:"label"`
-            Context testBytes `json:"context"`
-            Length  uint32    `json:"length"`
-            Secret  testBytes `json:"secret"`
-        } `json:"exporter"`
-    } `json:"epochs"`
+#[derive(Serialize, Deserialize, Debug, Clone, Default)]
+struct Exporter {
+    #[serde(with = "hex")]
+    label: Vec<u8>,
+    #[serde(with = "hex")]
+    context: Vec<u8>,
+    length: u32,
+    #[serde(with = "hex")]
+    secret: Vec<u8>,
 }
 
-func testKeySchedule(t *testing.T, tc *keyScheduleTest) {
-    initSecret := []byte(tc.InitialInitSecret)
-    for i, epoch := range tc.Epochs {
-        t.Logf("epoch %d", i)
+#[derive(Serialize, Deserialize, Debug, Clone, Default)]
+struct Epoch {
+    // Chosen by the generator
+    #[serde(with = "hex")]
+    tree_hash: Vec<u8>,
+    #[serde(with = "hex")]
+    commit_secret: Vec<u8>,
+    #[serde(with = "hex")]
+    psk_secret: Vec<u8>,
+    #[serde(with = "hex")]
+    confirmed_transcript_hash: Vec<u8>,
 
-        ctx := groupContext{
-            version:                 protocolVersionMLS10,
-            cipherSuite:             tc.CipherSuite,
-            groupID:                 GroupID(tc.GroupID),
-            epoch:                   uint64(i),
-            treeHash:                []byte(epoch.TreeHash),
-            confirmedTranscriptHash: []byte(epoch.ConfirmedTranscriptHash),
-        }
-        rawCtx, err := marshal(&ctx)
-        if err != nil {
-            t.Fatalf("marshal(groupContext) = %v", err)
-        } else if !bytes.Equal(rawCtx, []byte(epoch.GroupContext)) {
-            t.Errorf("marshal(groupContext) = %v, want %v", rawCtx, epoch.GroupContext)
-        }
+    // Computed values
+    #[serde(with = "hex")]
+    group_context: Vec<u8>,
+    #[serde(with = "hex")]
+    joiner_secret: Vec<u8>,
+    #[serde(with = "hex")]
+    welcome_secret: Vec<u8>,
+    #[serde(with = "hex")]
+    init_secret: Vec<u8>,
+    #[serde(with = "hex")]
+    sender_data_secret: Vec<u8>,
+    #[serde(with = "hex")]
+    encryption_secret: Vec<u8>,
+    #[serde(with = "hex")]
+    exporter_secret: Vec<u8>,
+    #[serde(with = "hex")]
+    epoch_authenticator: Vec<u8>,
+    #[serde(with = "hex")]
+    external_secret: Vec<u8>,
+    #[serde(with = "hex")]
+    confirmation_key: Vec<u8>,
+    #[serde(with = "hex")]
+    membership_key: Vec<u8>,
+    #[serde(with = "hex")]
+    resumption_psk: Vec<u8>,
 
-        joinerSecret, err := ctx.extractJoinerSecret(initSecret, []byte(epoch.CommitSecret))
-        if err != nil {
-            t.Errorf("extractJoinerSecret() = %v", err)
-        } else if !bytes.Equal(joinerSecret, []byte(epoch.JoinerSecret)) {
-            t.Errorf("extractJoinerSecret() = %v, want %v", joinerSecret, epoch.JoinerSecret)
-        }
+    #[serde(with = "hex")]
+    external_pub: Vec<u8>,
+    exporter: Exporter,
+}
 
-        welcomeSecret, err := extractWelcomeSecret(ctx.cipherSuite, joinerSecret, []byte(epoch.PSKSecret))
-        if err != nil {
-            t.Errorf("extractWelcomeSecret() = %v", err)
-        } else if !bytes.Equal(welcomeSecret, []byte(epoch.WelcomeSecret)) {
-            t.Errorf("extractWelcomeSecret() = %v, want %v", welcomeSecret, epoch.WelcomeSecret)
-        }
+#[derive(Default, Debug, Clone, Serialize, Deserialize)]
+struct KeyScheduleTest {
+    cipher_suite: u16,
+    #[serde(with = "hex")]
+    group_id: Vec<u8>,
+    #[serde(with = "hex")]
+    initial_init_secret: Vec<u8>,
+    epochs: Vec<Epoch>,
+}
 
-        epochSecret, err := ctx.extractEpochSecret(joinerSecret, []byte(epoch.PSKSecret))
-        if err != nil {
-            t.Fatalf("extractEpochSecret() = %v", err)
-        }
+fn key_schedule_test(
+    crypto_provider: &impl CryptoProvider,
+    cipher_suite: CipherSuite,
+    tc: &KeyScheduleTest,
+) -> Result<()> {
+    let mut init_secret: Bytes = tc.initial_init_secret.clone().into();
+    for (i, epoch) in tc.epochs.iter().enumerate() {
+        println!("epoch {}", i);
 
-        initSecret, err = ctx.cipherSuite.deriveSecret(epochSecret, secretLabelInit)
-        if err != nil {
-            t.Errorf("deriveSecret(init) = %v", err)
-        } else if !bytes.Equal(initSecret, []byte(epoch.InitSecret)) {
-            t.Errorf("deriveSecret(init) = %v, want %v", initSecret, epoch.InitSecret)
-        }
+        let ctx = GroupContext {
+            version: PROTOCOL_VERSION_MLS10,
+            cipher_suite,
+            group_id: tc.group_id.clone().into(),
+            epoch: i as u64,
+            tree_hash: epoch.tree_hash.clone().into(),
+            confirmed_transcript_hash: epoch.confirmed_transcript_hash.clone().into(),
+            extensions: vec![],
+        };
 
-        secrets := []struct {
-            label []byte
-            want  testBytes
-        }{
-            {secretLabelSenderData, epoch.SenderDataSecret},
-            {secretLabelEncryption, epoch.EncryptionSecret},
-            {secretLabelExporter, epoch.ExporterSecret},
-            {secretLabelExternal, epoch.ExternalSecret},
-            {secretLabelConfirm, epoch.ConfirmationKey},
-            {secretLabelMembership, epoch.MembershipKey},
-            {secretLabelResumption, epoch.ResumptionPSK},
-        }
-        for _, secret := range secrets {
-            sec, err := ctx.cipherSuite.deriveSecret(epochSecret, secret.label)
-            if err != nil {
-                t.Errorf("deriveSecret(%v) = %v", string(secret.label), err)
-            } else if !bytes.Equal(sec, []byte(secret.want)) {
-                t.Errorf("deriveSecret(%v) = %v, want %v", string(secret.label), sec, secret.want)
-            }
+        let raw_ctx = write(&ctx)?;
+        assert_eq!(raw_ctx.as_ref(), &epoch.group_context);
+
+        let joiner_secret =
+            ctx.extract_joiner_secret(crypto_provider, &init_secret, &epoch.commit_secret)?;
+        assert_eq!(joiner_secret.as_ref(), &epoch.joiner_secret);
+
+        let welcome_secret = extract_welcome_secret(
+            crypto_provider,
+            cipher_suite,
+            &joiner_secret,
+            &epoch.psk_secret,
+        )?;
+        assert_eq!(welcome_secret.as_ref(), &epoch.welcome_secret);
+
+        let epoch_secret =
+            ctx.extract_epoch_secret(crypto_provider, &joiner_secret, &epoch.psk_secret)?;
+
+        init_secret =
+            crypto_provider.derive_secret(cipher_suite, &epoch_secret, SECRET_LABEL_INIT)?;
+        assert_eq!(init_secret.as_ref(), &epoch.init_secret);
+
+        let secrets: Vec<(&[u8], &[u8])> = vec![
+            (SECRET_LABEL_SENDER_DATA, &epoch.sender_data_secret),
+            (SECRET_LABEL_ENCRYPTION, &epoch.encryption_secret),
+            (SECRET_LABEL_EXPORTER, &epoch.exporter_secret),
+            (SECRET_LABEL_EXTERNAL, &epoch.external_secret),
+            (SECRET_LABEL_CONFIRM, &epoch.confirmation_key),
+            (SECRET_LABEL_MEMBERSHIP, &epoch.membership_key),
+            (SECRET_LABEL_RESUMPTION, &epoch.resumption_psk),
+        ];
+
+        for secret in secrets {
+            let sec = crypto_provider.derive_secret(cipher_suite, &epoch_secret, secret.0)?;
+            assert_eq!(sec.as_ref(), secret.1);
         }
 
         // TODO: verify external pub, exporter secret
     }
+
+    Ok(())
 }
 
-func TestKeySchedule(t *testing.T) {
-    var tests []keyScheduleTest
-    loadTestVector(t, "testdata/key-schedule.json", &tests)
+fn test_key_schedule_with_crypto_provider(
+    tests: &[KeyScheduleTest],
+    crypto_provider: &impl CryptoProvider,
+) -> Result<()> {
+    for tc in tests {
+        let cipher_suite: CipherSuite = tc.cipher_suite.try_into()?;
+        println!("test_key_schedule {}:{}", cipher_suite, cipher_suite as u16);
 
-    for i, tc := range tests {
-        t.Run(fmt.Sprintf("[%v]", i), func(t *testing.T) {
-            testKeySchedule(t, &tc)
-        })
+        if crypto_provider.supports(cipher_suite).is_ok() {
+            key_schedule_test(crypto_provider, cipher_suite, tc)?;
+        }
     }
+
+    Ok(())
 }
 
+#[test]
+fn test_key_schedule() -> Result<()> {
+    let tests: Vec<KeyScheduleTest> = load_test_vector("test-vectors/key-schedule.json")?;
+
+    test_key_schedule_with_crypto_provider(&tests, &RingCryptoProvider {})?;
+    test_key_schedule_with_crypto_provider(&tests, &RustCryptoProvider {})?;
+
+    Ok(())
+}
+/*
 type transcriptHashesTest struct {
     CipherSuite cipherSuite `json:"cipher_suite"`
 

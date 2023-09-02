ab77b759e0bca09569ed85f0bd5c47dbf1c872fc
Rusty Rain <y@liu.mx>
2023-09-02 16:19:42 -0700

    add doc for tree_math

--- rmls/src/group/mod.rs
+++ rmls/src/group/mod.rs
@@ -1,5 +1,11 @@
 //! [RFC9420 Sec.11](https://www.rfc-editor.org/rfc/rfc9420.html#section-11) Group Creation and
 //! [RFC9420 Sec.12](https://www.rfc-editor.org/rfc/rfc9420.html#section-12) Group Evolution
+//!
+//! A group is always created with a single member, the "creator". Other members are then added to
+//! the group using the usual Add/Commit mechanism.
+//!
+//! Over the lifetime of a group, its membership can change, and existing members might want to
+//! change their keys in order to achieve post-compromise security.
 
 #[cfg(test)]
 mod group_test;
--- rmls/src/utilities/tree_math/mod.rs
+++ rmls/src/utilities/tree_math/mod.rs
@@ -1,4 +1,26 @@
 //! Tree Math
+//!
+//! The tree uses an array-based representation of complete balanced binary
+//! trees, as described in [RFC9420 Appendix C](https://www.rfc-editor.org/rfc/rfc9420.html#appendix-C).
+//! For example, a tree with 8 leaves:
+//! ```text
+//!                               X
+//!                               |
+//!                     .---------+---------.
+//!                    /                     \
+//!                   X                       X
+//!                   |                       |
+//!               .---+---.               .---+---.
+//!              /         \             /         \
+//!             X           X           X           X
+//!            / \         / \         / \         / \
+//!           /   \       /   \       /   \       /   \
+//!          X     X     X     X     X     X     X     X
+//!
+//!    Node: 0  1  2  3  4  5  6  7  8  9 10 11 12 13 14
+//!
+//!    Leaf: 0     1     2     3     4     5     6     7
+//! ```
 
 #[cfg(test)]
 mod tree_math_test;
@@ -7,37 +29,18 @@ use serde::{Deserialize, Serialize};
 
 use crate::utilities::error::*;
 
-// This uses an array-based representation of complete balanced binary
-// trees, as described in appendix C. For example, a tree with 8 leaves:
-//
-//                               X
-//                               |
-//                     .---------+---------.
-//                    /                     \
-//                   X                       X
-//                   |                       |
-//               .---+---.               .---+---.
-//              /         \             /         \
-//             X           X           X           X
-//            / \         / \         / \         / \
-//           /   \       /   \       /   \       /   \
-//          X     X     X     X     X     X     X     X
-//
-//    Node: 0  1  2  3  4  5  6  7  8  9 10 11 12 13 14
-//
-//    Leaf: 0     1     2     3     4     5     6     7
-
-// NumLeaves exposes operations on a tree with a given number of leaves.
+/// NumLeaves exposes operations on a tree with a given number of leaves.
 #[derive(Default, Debug, Copy, Clone, Eq, PartialEq, Serialize, Deserialize)]
 pub struct NumLeaves(pub(crate) u32);
 
 impl NumLeaves {
-    pub(crate) fn new(width: u32) -> Self {
+    /// Create a NumLeaves
+    pub fn new(width: u32) -> Self {
         Self(if width == 0 { 0 } else { (width - 1) / 2 + 1 })
     }
 
-    // width computes the minimum length of the array, ie. the number of nodes.
-    pub(crate) fn width(&self) -> u32 {
+    /// Compute the minimum length of the array, ie. the number of nodes.
+    pub fn width(&self) -> u32 {
         if self.0 == 0 {
             0
         } else {
@@ -45,13 +48,13 @@ impl NumLeaves {
         }
     }
 
-    // root returns the index of the root node.
-    pub(crate) fn root(&self) -> NodeIndex {
+    /// Return the index of the root node.
+    pub fn root(&self) -> NodeIndex {
         NodeIndex((1 << self.width().ilog2()) - 1)
     }
 
-    // parent returns the index of the parent node for a non-root node index.
-    pub(crate) fn parent(&self, x: NodeIndex) -> (NodeIndex, bool) {
+    /// Return the index of the parent node for a non-root node index.
+    pub fn parent(&self, x: NodeIndex) -> (NodeIndex, bool) {
         if x == self.root() {
             return (NodeIndex(0), false);
         }
@@ -61,8 +64,8 @@ impl NumLeaves {
         (NodeIndex(p), true)
     }
 
-    // sibling returns the index of the other child of the node's parent.
-    pub(crate) fn sibling(&self, x: NodeIndex) -> (NodeIndex, bool) {
+    /// Return the index of the other child of the node's parent.
+    pub fn sibling(&self, x: NodeIndex) -> (NodeIndex, bool) {
         let (p, ok) = self.parent(x);
         if !ok {
             return (NodeIndex(0), false);
@@ -74,8 +77,8 @@ impl NumLeaves {
         }
     }
 
-    // direct_path computes the direct path of a node, ordered from leaf to root.
-    pub(crate) fn direct_path(&self, mut x: NodeIndex) -> Vec<NodeIndex> {
+    /// Compute the direct path of a node, ordered from leaf to root.
+    pub fn direct_path(&self, mut x: NodeIndex) -> Vec<NodeIndex> {
         let mut path = vec![];
         loop {
             let (p, ok) = self.parent(x);
@@ -88,8 +91,8 @@ impl NumLeaves {
         path
     }
 
-    // copath computes the copath of a node, ordered from leaf to root.
-    pub(crate) fn copath(&self, x: NodeIndex) -> Result<Vec<NodeIndex>> {
+    /// Compute the copath of a node, ordered from leaf to root.
+    pub fn copath(&self, x: NodeIndex) -> Result<Vec<NodeIndex>> {
         let mut path = self.direct_path(x);
         if path.is_empty() {
             return Ok(vec![]);
@@ -110,27 +113,32 @@ impl NumLeaves {
     }
 }
 
-// NodeIndex is the index of a node in a tree.
+/// NodeIndex is the index of a node in a tree.
 #[derive(Default, Debug, Copy, Clone, Eq, PartialEq, Serialize, Deserialize)]
 pub struct NodeIndex(pub(crate) u32);
 
 impl NodeIndex {
-    // is_leaf returns true if this is a leaf node, false if this is an intermediate
+    /// Create a NodeIndex
+    pub fn new(v: u32) -> Self {
+        Self(v)
+    }
+
+    /// Return true if this is a leaf node, false if this is an intermediate
     // node.
-    pub(crate) fn is_leaf(&self) -> bool {
+    pub fn is_leaf(&self) -> bool {
         self.0 % 2 == 0
     }
 
-    // leaf_index returns the index of the leaf from a node index.
-    pub(crate) fn leaf_index(&self) -> (LeafIndex, bool) {
+    /// Return the index of the leaf from a node index.
+    pub fn leaf_index(&self) -> (LeafIndex, bool) {
         if !self.is_leaf() {
             return (LeafIndex(0), false);
         }
         (LeafIndex(self.0 >> 1), true)
     }
 
-    // left returns the index of the left child for an intermediate node index.
-    pub(crate) fn left(&self) -> (NodeIndex, bool) {
+    /// Return the index of the left child for an intermediate node index.
+    pub fn left(&self) -> (NodeIndex, bool) {
         let lvl = self.level();
         if lvl == 0 {
             return (NodeIndex(0), false);
@@ -139,8 +147,8 @@ impl NodeIndex {
         (NodeIndex(l), true)
     }
 
-    // right returns the index of the right child for an intermediate node index.
-    pub(crate) fn right(&self) -> (NodeIndex, bool) {
+    /// Return the index of the right child for an intermediate node index.
+    pub fn right(&self) -> (NodeIndex, bool) {
         let lvl = self.level();
         if lvl == 0 {
             return (NodeIndex(0), false);
@@ -149,9 +157,8 @@ impl NodeIndex {
         (NodeIndex(r), true)
     }
 
-    // children returns the indices of the left and right children for an
-    // intermediate node index.
-    pub(crate) fn children(&self) -> (NodeIndex, NodeIndex, bool) {
+    /// Returns the indices of the left and right children for an intermediate node index.
+    pub fn children(&self) -> (NodeIndex, NodeIndex, bool) {
         let (l, ok) = self.left();
         if !ok {
             return (NodeIndex(0), NodeIndex(0), false);
@@ -160,9 +167,9 @@ impl NodeIndex {
         (l, r, true)
     }
 
-    // level returns the level of a node in the tree. Leaves are at level 0, their
-    // parents are at level 1, etc.
-    pub(crate) fn level(&self) -> u32 {
+    /// Return the level of a node in the tree. Leaves are at level 0, their
+    /// parents are at level 1, etc.
+    pub fn level(&self) -> u32 {
         if self.0 & 1 == 0 {
             return 0;
         }
@@ -174,16 +181,23 @@ impl NodeIndex {
     }
 }
 
+/// LeafIndex is the index of a leave in a tree.
 #[derive(Default, Debug, Copy, Clone, Eq, PartialEq, Hash, Serialize, Deserialize)]
 pub struct LeafIndex(pub(crate) u32);
 
 impl LeafIndex {
-    // NodeIndex returns the index of the node from a leaf index.
-    pub(crate) fn node_index(&self) -> NodeIndex {
+    /// Create a LeafIndex
+    pub fn new(v: u32) -> Self {
+        Self(v)
+    }
+
+    /// Return the index of the node from a leaf index.
+    pub fn node_index(&self) -> NodeIndex {
         NodeIndex(2 * self.0)
     }
 }
 
-pub(crate) fn is_power_of_two(x: u32) -> bool {
+/// Check whether input value is power of 2
+pub fn is_power_of_two(x: u32) -> bool {
     x != 0 && (x & (x - 1) == 0)
 }

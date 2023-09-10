cd5756f4fdcca731e9279d7e27ab1c7a070e078e
yngrtc <y@ngr.tc>
2023-09-10 10:14:25 -0700

    doc crypto config

--- rmls/src/crypto/config.rs
+++ rmls/src/crypto/config.rs
@@ -3,8 +3,15 @@ use crate::framing::ProtocolVersion;
 
 #[derive(Default, Debug, Copy, Clone, Eq, PartialEq)]
 pub struct CryptoConfig {
-    pub version: ProtocolVersion,
-    pub cipher_suite: CipherSuite,
+    pub(crate) version: ProtocolVersion,
+    pub(crate) cipher_suite: CipherSuite,
+}
+
+impl CryptoConfig {
+    /// Create a crypto config builder
+    pub fn builder() -> CryptoConfigBuilder {
+        CryptoConfigBuilder::new()
+    }
 }
 
 #[derive(Default, Debug, Copy, Clone)]
@@ -14,20 +21,24 @@ pub struct CryptoConfigBuilder {
 }
 
 impl CryptoConfigBuilder {
+    /// Create a crypto config
     pub fn new() -> Self {
         Self::default()
     }
 
+    /// Build with version
     pub fn with_version(mut self, version: ProtocolVersion) -> Self {
         self.version = version;
         self
     }
 
+    /// Build with cipher suite
     pub fn with_cipher_suite(mut self, cipher_suite: CipherSuite) -> Self {
         self.cipher_suite = cipher_suite;
         self
     }
 
+    /// Finalize and build the crypto config
     pub fn build(self) -> CryptoConfig {
         CryptoConfig {
             version: self.version,
